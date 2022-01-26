373348f57810d93c14d2240ecf9d84472d266279
Meryem <89212084+meriemsermoune@users.noreply.github.com>
2022-01-26 10:33:20 -0600

    Add files via upload

---
+++ BuisnessData.java
@@ -0,0 +1,13 @@
+package data;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BuisnessData {
+    public static List<String> getExpectedAppNames(){
+        return Arrays.asList("Find Patient Record", "Active Visits", "Awaiting Admission","Register a patient"
+                ,"Capture Vitals", "Appointment Scheduling","Register a patient","Reports","Data Management","Configure Metadata"
+                ,"System Administration");
+
+    }
+}
